44e0e4234a787aa8b6e3b67b76d82124f318a0ee
Dayana Ivette <63428525+dayanaivette@users.noreply.github.com>
2020-05-12 18:02:15 -0600

    Parcial

--- GobiernoEmergencia/Program.cs
+++ GobiernoEmergencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GobiernoEmergencia.Vista;
 
 namespace GobiernoEmergencia
 {
@@ -16,7 +17,7 @@ namespace GobiernoEmergencia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Administrador());
+            Application.Run(new Consultar());
         }
     }
 }
